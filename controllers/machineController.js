d95ef95caf0e912d2241b31716f1956075def6c4
fajartc02 <fajartc02@gmail.com>
2023-09-30 07:12:00 +0700

    add main parameters column condition

--- controllers/machineController.js
+++ controllers/machineController.js
@@ -2,20 +2,46 @@ const response = require('../helpers/response')
 const query = require('../helpers/queryModule')
 const uuidToId = require('../helpers/UUIDtoID')
     // const idToUUID = require('../helpers/idToUUID')
-const { tb_m_machines, tb_m_lines, v_mc_params } = require('../config/table')
+const { tb_m_machines, tb_m_lines, tb_m_mc_parameters } = require('../config/table')
 const getLastId = require('../helpers/getLastId')
+const { v4 } = require('uuid')
 
 
 module.exports = {
     insertDB: async(req, res) => {
         try {
+            const { param_ids } = req.body
             req.body.machine_id = await getLastId(tb_m_machines, 'machine_id')
             req.body.uuid = req.uuid
             req.body.line_id = await uuidToId(tb_m_lines, 'line_id', req.body.line_id)
             req.body.created_by = req.user.user_nm
-                // Condition for add param_id to mc_params [id, id]
-            let resp = await query.insertDb(tb_m_machines, req.body)
-            if (resp) response.success(res, 'success add machine')
+
+            // Condition for add param_id to mc_params [id, id]
+            // create parameter
+            if (param_ids) {
+                var containerQuery = []
+                let mc_param_lastId = await getLastId(tb_m_mc_parameters, 'mc_param_id')
+                var mapMcParam = await param_ids.map((param, i) => {
+                    return {
+                        mc_param_id: mc_param_lastId + i,
+                        uuid: v4(),
+                        client_hdl: param,
+                        machine_id: req.body.machine_id
+                    }
+                })
+                for (let i = 0; i < mapMcParam.length; i++) {
+                    const element = mapMcParam[i];
+                    containerQuery.push(`(${element.mc_param_id}, "${element.uuid}", "${element.machine_id}", "${element.client_hdl}")`)
+                }
+                var queryMcParam = `INSERT INTO tb_m_mc_parameters(mc_param_id, uuid, machine_id, param_id) VALUES ${containerQuery.join(',')}`
+            }
+            delete req.body.param_ids
+            let respMc = await query.insertDb(tb_m_machines, req.body)
+            if (param_ids) await query.customDb(queryMcParam)
+
+
+
+            if (respMc) response.success(res, 'success add machine')
         } catch (error) {
             console.log(error);
             response.failed(res, 'Error add machine')
